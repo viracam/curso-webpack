30d6a12f216773a30d78a082639005092e895f95
Camilo Viracachá <viracam@gmail.com>
2022-11-04 10:18:07 -0500

    loaders para html, css y sass importados en el index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+    mode: 'production',
     entry: './src/index.js',
     output:{
         path: path.resolve(__dirname, 'dist'),
@@ -10,14 +13,43 @@ module.exports = {
         extensions: ['.js','.jsx']
     },
     module: {
-        rules:[{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use:{
-                loader: 'babel-loader',
+        
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\..html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
             }
-        }]
+        ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
     devServer: {
         static: path.join(__dirname, 'dist'),
         compress: true,
